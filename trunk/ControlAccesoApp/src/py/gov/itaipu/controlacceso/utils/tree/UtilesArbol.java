defbfdbd6227179b7f6f553169e12bc354bda5d0
ferboy86 <ferboy86@187bf257-e0c2-7b3b-c7e3-dd00301f3e39>
2013-02-13 18:17:46 +0000

    MODIFICACION DE CREACION ARBOL

--- trunk/ControlAccesoApp/src/py/gov/itaipu/controlacceso/utils/tree/UtilesArbol.java
+++ trunk/ControlAccesoApp/src/py/gov/itaipu/controlacceso/utils/tree/UtilesArbol.java
@@ -22,65 +22,45 @@ public class UtilesArbol {
     public UtilesArbol() {
         
     }
-    
-    public static DefaultMutableTreeNode crearArbol(String tituloArbol,boolean incluyePersonas){
+     public static DefaultMutableTreeNode crearArbol (String tituloArbol,boolean incluyePersonas){
             DefaultMutableTreeNode root = new DefaultMutableTreeNode(tituloArbol);
             CRUDAction<Organizacion> organizacionAction = new CRUDAction<Organizacion>(new Organizacion());
+            DefaultMutableTreeNode node;
             
             List<Organizacion> listaOrganizacionesArbolAux = new ArrayList<Organizacion>();
             
-            DefaultMutableTreeNode node;
-            DefaultMutableTreeNode nodePadre;
-            
-            listaOrganizacionesArbolAux = organizacionAction.findByNamedQuery("Organizacion.findOrgMaxNivel");
-            int maxNivel = listaOrganizacionesArbolAux.get(0).getNivelOrganigrama().intValue();
-             
             //SE AGREGAN TODAS LAS ORGANIZACIONES AL ARBOL
-            listaOrganizacionesArbolAux = organizacionAction.findByNamedQuery("Organizacion.findAllInterna");
-            for (int i = 0; i < listaOrganizacionesArbolAux.size(); i++) {
-                Organizacion organizacion = listaOrganizacionesArbolAux.get(i);
-                nodePadre = new DefaultMutableTreeNode(organizacion,true);
-                root.add(nodePadre);   
-                if (incluyePersonas) {
-                    ///SE AGREGAN LAS PERSONAS DEPENDIENTES DE CADA DEPARTAMENTO
-                    List<Persona> listaPersonasDepto = new ArrayList<Persona>();
-                    listaPersonasDepto = organizacion.getPersonas();
-                    if (listaPersonasDepto!=null && listaPersonasDepto.size()>0) {
-                        for (int j = 0; j < listaPersonasDepto.size(); j++) {
-                            Persona persona1 = listaPersonasDepto.get(j);
-                            node = new DefaultMutableTreeNode(persona1,false);
-                            nodePadre.add(node);
-                        }
-                    }
-                }
-           }
-               
-            // SE RECORREN LAS ORGANIZACIONES DESDE NIVEL MAXIMO A MINIMO Y SE INTRODUCEN NODOS HIJOS
-                int cantOrganizaciones = root.getChildCount();
-                for (int b = maxNivel ; b > 0 ; b--) {
-                     for (int j = 0; j < cantOrganizaciones; j++) {
-                         node = (DefaultMutableTreeNode) root.getChildAt(j);
-                         Organizacion org = (Organizacion)node.getUserObject();
-                         if (org.getOrganizacionPadre()!=null) {
-                                if (org.getNivelOrganigrama()==b) {
-                                //buscamos su padre
-                                for (int h = 0; h < cantOrganizaciones; h++) {
-                                   nodePadre = (DefaultMutableTreeNode) root.getChildAt(h);
-                                   Organizacion orgPadre = (Organizacion)nodePadre.getUserObject();
-                                    if (org.getOrganizacionPadre().getId() == orgPadre.getId()) {
-                                        //Ingreso el Nodo Hijo en el nodo padre y resto 1 a la cantidad inicial de nodos del nivel
-                                        nodePadre.add(node);
-                                        cantOrganizaciones = cantOrganizaciones - 1;
-                                        j = j - 1;
-                                        break;
-                                    }
-                                }
-                             }
-                         }
-                     }
-                }
-    return root; 
-    }
+            listaOrganizacionesArbolAux = organizacionAction.findByNamedQuery("Organizacion.findOrganizacionPadre");
+            Organizacion orgPadre = listaOrganizacionesArbolAux.get(0);
+            node = new DefaultMutableTreeNode(orgPadre,true);
+            root.add(node);
+            agregarhijos(node,incluyePersonas);
+            return root;
+     }
+     private static void agregarhijos(DefaultMutableTreeNode nodo, boolean incluyePersonas){
+        if(incluyePersonas) {
+                 agregarPersonas(nodo);
+        }
+        DefaultMutableTreeNode node;
+        Organizacion org = (Organizacion) nodo.getUserObject();
+        List<Organizacion> hijas = org.getOrganizacionesHijas();
+         for (Organizacion organizacion : hijas) {
+             node = new DefaultMutableTreeNode(organizacion,true);
+             nodo.add(node);
+              agregarhijos(node,incluyePersonas);
+         }
+     }
+     private static void agregarPersonas(DefaultMutableTreeNode nodo){
+        DefaultMutableTreeNode node;
+        Organizacion org = (Organizacion) nodo.getUserObject();
+        List<Persona> personas = org.getPersonas();
+         for (Persona persona : personas) {
+             node = new DefaultMutableTreeNode(persona,true);
+             nodo.add(node);
+         }
+     }
+    
+    
     
     
     
